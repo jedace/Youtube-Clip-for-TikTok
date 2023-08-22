991cdf5afde4c2a0936419d9de1d72d95531dcbf
jedace <89285386+jedace@users.noreply.github.com>
2023-08-22 15:55:07 -0500

    Unique naming system
    Imported datetime to allow unique naming of files. This makes it so that when doing clips from different videos, the first clip titled "clip_0" doesn't get overwritten

--- tiktokclip.py
+++ tiktokclip.py
@@ -1,19 +1,23 @@
 from pytube import YouTube
 from moviepy.video.io.VideoFileClip import VideoFileClip
+from datetime import datetime
 
 # Get the URL of the YouTube video from the user
 video_url = input("Enter the URL of the YouTube video: ")
 
-# Download the video from YouTube
+# Download the highest resolution stream from YouTube
 yt = YouTube(video_url)
 video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
 
-# Cut the video into x-second long clips, where i = clip length
+# Cut the video into 61-second long clips
 video_clip = VideoFileClip(video)
 duration = video_clip.duration
 
-for i in range(0, int(duration), 98):
+# Get the current timestamp
+timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+
+for i in range(0, int(duration), 61):
     start_time = i
-    end_time = min(i + 98, duration)
+    end_time = min(i + 61, duration)
     clip = video_clip.subclip(start_time, end_time)
-    clip.write_videofile(f"clip_{i}.mp4")
+    clip.write_videofile(f"clip_{timestamp}_{i}.mp4")
